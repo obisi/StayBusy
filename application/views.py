b5f66bdb7fade52558ddb29b5cba3435bbfb6aef
obisi <ollibisi@yahoo.com>
2018-08-17 16:40:06 +0300

    korjaus viikkoon 4: poistetaan ominaisuus, jolla näytetään käyttäjän pisin juoksu etusivulla

--- application/views.py
+++ application/views.py
@@ -3,15 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 from application import app
 from application.auth.models import User
-from flask_login import login_required, current_user
 
 
 @app.route("/")
 def hello():
     juoksu = User.pisin_juoksu()
     juoksu=juoksu[0]
-    juoksuOma = User.pisin_juoksu_oma(current_user.id)
-    juoksuOma = juoksuOma[0]
-    return render_template("index.html", juoksu = juoksu["matkaString"], omajuoksu=juoksuOma["matkaString"])
+    return render_template("index.html", juoksu = juoksu["matkaString"])
 
 
